59f127ac96c159061711db5d25f74966db9ea3ef
abhishekrawool <121244445+abhishekrawool@users.noreply.github.com>
2023-12-01 15:28:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var http = require('http');
-var hostname = 'localhost';
-var port = 8000;
-var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function(req, res) {
+    res.writeHead(200);
+    res.end("Hello World from Node.js HTTP Server");
+}
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    // this is for testing 
+    console.log(`Server is running on http://${host}:${port}`);
+}); 
